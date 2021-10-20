256e7b55d2d1386a16565ea8b028ea52316623ea
Stan Hung <ak47andy@gmail.com>
2021-10-20 16:56:23 +0800

    fix tab

--- app.py
+++ app.py
@@ -37,13 +37,13 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	msg = event.message.text
-	r = '我這邊正常，你要不要再試一次'
+    msg = event.message.text
+    r = '我這邊正常，你要不要再試一次'
 
-	if msg == 'hi':
-		r = 'hi'
-	elif: msg = '你吃飯了嗎':
-		r = '還沒，你要請客嗎?'
+    if msg in ['hi', 'Hi', '你好', '您好', '安安']:
+        r = '嗨'
+    elif: msg = '你吃飯了嗎':
+        r = '還沒，你要請客嗎?'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
