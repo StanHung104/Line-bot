065911c97630a50d8a8f0c539aa8d18c94b7fae3
Stan Hung <ak47andy@gmail.com>
2021-10-20 17:00:33 +0800

    fix app

--- app.py
+++ app.py
@@ -42,8 +42,9 @@ def handle_message(event):
 
     if msg in ['hi', 'Hi', '你好', '您好', '安安']:
         r = '嗨'
-    elif: msg = '你吃飯了嗎':
+    elif msg == '你吃飯了嗎':
         r = '還沒，你要請客嗎?'
+        
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
