13821e3b3267fba0e111128fa3fb5d600e158c41
Stan Hung <ak47andy@gmail.com>
2021-10-20 17:08:21 +0800

    update

--- app.py
+++ app.py
@@ -40,14 +40,21 @@ def handle_message(event):
     msg = event.message.text
     r = '我這邊正常，你要不要再試一次'
 
-    if msg in ['hi', 'Hi', '你好', '您好', '安安']:
+    if msg in ['hi', 'Hi', '你好', '您好', '安安', 'HI']:
         r = '嗨'
     elif msg == '你吃飯了嗎':
         r = '還沒，你要請客嗎?'
-        
+    elif msg == in ['你是誰', '你是誰?', '?', 'who are you']:
+    	r = '我是Stan建置的機器人?'
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
+
+#上傳圖片
+    # sticker_message = StickerSendMessage(
+    # package_id='1',
+    # sticker_id='1')
 
 
 if __name__ == "__main__":
